fe260198ace0136b0567da48200c286564e9b5bd
Tochukwu Nwachukwu <truetochukz@gmail.com>
2022-10-01 10:26:44 +0200

    HelloCDK Stack

---
+++ AWS-CDK/chp2/hello-cdk/lib/hello-cdk-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3} from 'aws-cdk-lib';
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'Bucket01', {
+      versioned: true,
+      bucketName: 'chucks-hello-cdk-bucket',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
