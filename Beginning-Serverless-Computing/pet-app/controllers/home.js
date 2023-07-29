cad5d6b539eaa479a4df07605249deeb9233dfb8
Tochukwu Nwachukwu <truetochukz@gmail.com>
2023-07-29 10:34:04 +0100

    Moved "Beginning-Serverless" to Serverless repository

--- Beginning-Serverless-Computing/pet-app/controllers/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  return res.send('Welcome to the Pet App');
-});
-
-module.exports = router; 
